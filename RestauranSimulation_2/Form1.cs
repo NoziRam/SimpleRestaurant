5e297607bf8afe569ccc8d6c4138f5a9ccf065ea
NoziRam <1000dreams000@gmail.com>
2023-08-06 08:36:11 +0500

    coding interface

--- RestauranSimulation_2/Form1.cs
+++ RestauranSimulation_2/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestaurantSimulation_2;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,57 @@ namespace RestauranSimulation_2
 {
     public partial class Form1 : Form
     {
+        Server server;
         public Form1()
         {
             InitializeComponent();
+            server = new Server();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+             int chickenQuantity = Convert.ToInt32(textBox1.Text);
+             int eggQuantity = Convert.ToInt32(textBox2.Text);
+             string drink = comboBox1.Text;
+             Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int? quality = server.SendAll();
+                label3.Text = $"Egg quality:{quality}";
+            }
+            catch (Exception ex)
+            {
+              MessageBox.Show(ex.Message);
+            }
+        
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        { 
+        
+        
+        }
+        private void Clear()
+        {
+            textBox1.Text = "0";
+            textBox2.Text = "0";
+            comboBox1.Text = "";
         }
     }
 }
