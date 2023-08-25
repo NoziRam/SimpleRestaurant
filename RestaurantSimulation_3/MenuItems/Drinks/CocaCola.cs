5d7d00f670c20fa6cbd91d8c8029bd108193a8bd
NoziRam <1000dreams000@gmail.com>
2023-08-25 21:38:59 +0500

    Added new classes

---
+++ RestaurantSimulation_3/MenuItems/Drinks/CocaCola.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantSimulation_3.MenuItems.Drinks
+{
+    class CocaCola
+    {
+    }
+}
