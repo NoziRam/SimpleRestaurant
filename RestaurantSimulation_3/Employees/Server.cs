7da33e5125e401c641b0d6b41f7189d4e390293b
NoziRam <1000dreams000@gmail.com>
2023-09-03 11:31:26 +0500

    Add method AddNewRequest

--- RestaurantSimulation_3/Employees/Server.cs
+++ RestaurantSimulation_3/Employees/Server.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RestaurantSimulation_3.MenuItems.Foods;
+using RestaurantSimulation_3.MenuItems.Drinks;
 
 namespace RestaurantSimulation_3.Employees
 {
-   public class Server
+    public class Server
     {
+        TableRequests tableRequests;
+        Cook cook;
+        public Server()
+        {
+            tableRequests = new TableRequests();
+            cook = new Cook();
+        }
+        int customerIndex = 0;
+        public void AddNewRequest(int chickenCount, int eggCount, string drink)
+        {
+            for (int i = 0; i < chickenCount; i++)
+            {
+                tableRequests.Add(customerIndex, new Chicken(chickenCount));
+            }
+            for (int i = 0; i < eggCount; i++)
+            {
+                tableRequests.Add(customerIndex, new Egg(eggCount));
+            }
+            if (drink == "CocaCola")
+                tableRequests.Add(customerIndex, new CocaCola());
+            else if (drink == "Pepsi")
+                tableRequests.Add(customerIndex, new Pepsi());
+            else tableRequests.Add(customerIndex, new Tea());
+        
+        }
     }
 }
